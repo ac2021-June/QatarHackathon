b955e0db743520abeb2a74f816dc07df13059a1e
Pallavi Chaudhari <pallavi.chaudhari@aircanada.ca>
2023-11-25 23:02:35 -0500

    last updates

--- ClientApp/src/app/1notocrecord/1notocrecord-data.component.ts
+++ ClientApp/src/app/1notocrecord/1notocrecord-data.component.ts
@@ -14,14 +14,14 @@ import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-iataonerecord-data',
-  templateUrl: './iataonerecord-data.component.html',
-  styleUrls: ['./iataonerecord-data.component.css'],
+  selector: 'app-notocrecord-data',
+  templateUrl: './notocrecord-data.component.html',
+  styleUrls: ['./notocrecord-data.component.css'],
 
  /* styleUrls: ['./dragndrop-data.component.css'],*/
 
 })
-export class IATAOneRecordDataComponent {
+export class NotocRecordDataComponent {
   public notocrecords: NOTOCRecord[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
@@ -30,7 +30,6 @@ export class IATAOneRecordDataComponent {
     }, error => console.error(error));
   }
 }
-
 
 interface NOTOCRecord {
   "itemNumber": number;
@@ -59,22 +58,3 @@ interface NOTOCRecord {
   "captainSignature": string;
 }
 
-
-//export class DragndropDataComponent {
-//  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-//  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-//  drop(event: CdkDragDrop<string[]>) {
-//    if (event.previousContainer === event.container) {
-//      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-//    } else {
-//      transferArrayItem(
-//        event.previousContainer.data,
-//        event.container.data,
-//        event.previousIndex,
-//        event.currentIndex,
-//      );
-//    }
-//  }
-//}
--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -3,21 +3,45 @@ import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
+  templateUrl: './fetch-data.component.html',
+    styleUrls: ['./fetch-data.component.css'],
+
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[] = [];
+  public notocrecords: NOTOCRecord[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<NOTOCRecord[]>(baseUrl + 'notocrecord').subscribe(result => {
+      this.notocrecords = result;
     }, error => console.error(error));
   }
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+
+interface NOTOCRecord {
+  "itemNumber": number;
+  "uldNumber": string;
+  "properShippingName": string;
+  "originStation": string;
+  "offloadStation": string;
+  "finalDestination": string;
+  "airWaybillNumber": string;
+  "hazardClassificationId": string;
+  "unNumber": string;
+  "additionalHazardClassificationId": string;
+  "minimumQuantityPerShipment": number;
+  "transportIndexNumericdpback": number;
+  "unitOfMeasure": string;
+  "numberOfPackages": number;
+  "packagingDangerLevelCode": string;
+  "dgRaTypeCode": string;
+  "cargoAircraftOnly": boolean;
+  "cargoIMPCode": string;
+  "locationOnAircraft": string;
+  "temperatureRequirement": string;
+  "stateExemption": string;
+  "remarks": string;
+  "loadingPersonnelSignature": string;
+  "captainSignature": string;
 }
+
